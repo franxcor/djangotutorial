45050330d68094092daab522ed43b7230e53783c
franxcor <129801200+franxcor@users.noreply.github.com>
2025-05-02 10:43:01 -0400

    finished up part 2

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+import datetime
+from django.db import models
+from django.utils import timezone
 # Create your models here.
 
 ##poll question
@@ -7,8 +9,20 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
+    #string method to see the values & not just id
+    def __str__(self) :
+        return self.question_text
+    
+    #method to check if published within one day 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 ##poll options
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0) 
+
+    #string method to see the values & not just id
+    def __str__(self) :
+        return self.choice_text
