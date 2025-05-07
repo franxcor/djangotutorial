2715b85f2855f70af7d6fae822b2b6aaabfe5489
franxcor <129801200+franxcor@users.noreply.github.com>
2025-05-06 20:50:44 -0400

    push before I code shuffle

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
 
 from django.http import Http404 #original if question id doesn't exist method
 
+from django.db.models import F
+from django.urls import reverse
 from django.template import loader
 # Create your views here.
 def index(request) :
@@ -31,8 +33,25 @@ def detail(request, question_id):
     '''
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    # check valid question
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        #since it is a radio, if the choice is not valid, it would be no select
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice." #this fills in the error message we put in earler
+            },
+        )
+    else:
+        selected_choice.votes=F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
