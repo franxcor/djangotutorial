45f6d45164e85612d59e8d65a32b959df0cacb3b
franxcor <129801200+franxcor@users.noreply.github.com>
2025-05-06 21:04:23 -0400

    finishing part 4

--- polls/urls.py
+++ polls/urls.py
@@ -3,15 +3,15 @@ from . import views
 app_name = "polls" # since we only have one app in our workspace
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
 
     #these paths automatically run the functsion that are defined in the views file
     
     # /polls/num/
-    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
 
     # /polls/num/results
-    path("<int:question_id>/results", views.results, name="results"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
 
     # /polls/num/vote
     path("<int:question_id>/vote/", views.vote, name="vote")
